81422906202da29bc0151e9c6fde77f88ea55515
pengsongyou <psy920710@gmail.com>
2016-05-20 15:48:17 +0200

    Finishes lab5 coding part

--- lab5/q_learning.m
+++ lab5/q_learning.m
@@ -1,8 +1,17 @@
 function Q = q_learning(map,q_goal,alpha,gamma,epsilon,n_episodes,n_iterations)
     [h,w]= size(map);
     Q = zeros(h,w,4);
-    action = [-1,0;0,1;1,0;0,-1];
+    action = [-1,0;0,1;1,0;0,-1];% Four actions 
+    
+    n_test = 200;
+    rewards_test = zeros(ceil(n_episodes/n_test), 1);
+    t = 1;
     for n = 1 : n_episodes
+        
+        % Testing the learnt policy
+        if mod(n, n_test) == 0 && n ~= n_episodes 
+            t = t + 1;
+        end
         
         % Initialize s randomly in any free cell
         s1 = randi(h);
@@ -42,7 +51,16 @@ function Q = q_learning(map,q_goal,alpha,gamma,epsilon,n_episodes,n_iterations)
                 Q(s1, s2, a_idx) = Q(s1,s2, a_idx) + alpha * (-1 + gamma * max(Q(s1, s2,:)) - Q(s1, s2, a_idx));
             end
         end
+        rewards_test(t) = rewards_test(t) - m;
     end
+    
+    % Plot the effectiveness
+    figure; hold on;
+    
+    xa = 0:n_test: n_episodes;
+    plot(xa, [-50, rewards_test'./n_test],'-');
+    title('Evolution of effectiveness');
+    hold off;
     
     % Graphical representation of the State Value Function V
     V = zeros(size(Q,1),size(Q,2));
@@ -51,6 +69,7 @@ function Q = q_learning(map,q_goal,alpha,gamma,epsilon,n_episodes,n_iterations)
             V(i,j) = max(Q(i,j,:));
         end
     end
+    
     
     figure;imagesc(V);
     colormap jet
@@ -62,23 +81,25 @@ function Q = q_learning(map,q_goal,alpha,gamma,epsilon,n_episodes,n_iterations)
     for i = 1 : size(Q,1)
         for j = 1 : size(Q,2)
             if Q(i,j,1) == 0 && Q(i,j,2) == 0 && Q(i,j,3) == 0 && Q(i,j,4) == 0
-                plot(j,15-i,'ro');
+                plot(j,h+1 -i,'ro');
                 continue;
             end
             action = find(Q(i,j,:) == max(Q(i,j,:)));
             action = action(1);
             switch action
                 case 1
-                    text(j,h + 1 - i,'\uparrow','HorizontalAlignment','center','FontSize',20);
+                    text(j,h + 1 - i,'\uparrow','HorizontalAlignment','center','FontSize',10);
                 case 2
-                    text(j,h + 1 - i,'\rightarrow','HorizontalAlignment','center','FontSize',20);
+                    text(j,h + 1 - i,'\rightarrow','HorizontalAlignment','center','FontSize',10);
                 case 3
-                    text(j,h + 1 - i,'\downarrow','HorizontalAlignment','center','FontSize',20);
+                    text(j,h + 1 - i,'\downarrow','HorizontalAlignment','center','FontSize',10);
                 case 4  
-                    text(j,h + 1 - i,'\leftarrow','HorizontalAlignment','center','FontSize',20);
+                    text(j,h + 1 - i,'\leftarrow','HorizontalAlignment','center','FontSize',10);
             end
         end
     end
     plot(q_goal(1),h + 1 - q_goal(2),'g*');
     axis off
+    title('Optimal Policy');
+    hold off;
 end
